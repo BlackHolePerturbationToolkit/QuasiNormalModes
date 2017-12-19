fca27565899e11f5c3c02165eebc291fd5785a52
otoolec2 <otoolec2@tcd.ie>
2017-12-19 15:35:16 +0000

    Moving to top level directory

---
+++ PacletInfo.m
@@ -0,0 +1,21 @@
+(* ::Package:: *)
+
+Paclet[
+  Name -> "QuasiNormalModes",
+  Version -> "0.1.0",
+  MathematicaVersion -> "11+",
+  Creator -> "Conor O'Toole",
+  Description -> "Code to return the Quasinormal Mode frequencies of a rotating or non-rotating frequency.",
+  Extensions -> {
+    { "Kernel",
+	  "Context" -> {"QuasiNormalModes`"}
+	},
+
+    {"Documentation",
+     Language -> "English", 
+     MainPage -> "Guides/QuasiNormal Modes",
+     Resources -> 
+     	{"Guides/QuasiNormal Modes"}
+    }
+  }
+]
