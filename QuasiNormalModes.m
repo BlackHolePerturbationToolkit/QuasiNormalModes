dcd94bf82580df743a4158cf42aea61128d32e37
otoolec2 <conor.o-toole.1@ucdconnect.ie>
2019-01-05 12:26:15 +0000

    Schwarzschild initial seed updated to include spectral method

--- QuasiNormalModes.m
+++ QuasiNormalModes.m
@@ -22,12 +22,14 @@ M = 1; (* Mass. This is the standard convention *)
 k1[m_, s_] := 1/2 Abs[m-s];
 k2[m_, s_] := 1/2 Abs[m+s];
 
+(* Functions for the Continued Fraction used in Leaver's method for Schwarzschild *)
 \[Alpha][i_, \[Omega]_]  := i^2 - 4 M I \[Omega] i + 2 i - 4 M I \[Omega] + 1;
 \[Delta][i_, \[Omega]_,s_, l_] := -2 i^2 - (2 - 16 M I \[Omega])i + 32 M^2 \[Omega]^2 + 8 M I \[Omega] - l(l + 1) + \[Beta][s];
 \[Gamma][i_, \[Omega]_, s_] := i^2 - 8 M I \[Omega] i - 16 M^2 \[Omega]^2 - \[Beta][s] - 1;
 
 b[a_] := b[a] = Sqrt[4 M^2 - 4 a^2];
 
+(* Functions for the Continued Fraction used in Leaver's method for Kerr *)
 \[Alpha]freq[i_,\[Omega]_, s_, m_, a_]:= i^2 + (2-s-2 M I \[Omega] -2 I/b[a] (2 M^2 \[Omega] - a m))i + 1 - s - 2 M I \[Omega] -2 I/b[a] (2 M^2 \[Omega] - a m);
 \[Beta]freq[i_,\[Omega]_, Alm_, s_, m_, a_]:= -2 i^2 +(-2 + 2(4 M + b[a]) I \[Omega] + 4 I/b[a] (2 M^2 \[Omega] - a m)) i + (16 M^2 + 4 M b[a] - a^2) \[Omega]^2 - s - 1 - 2 a m \[Omega] - Alm +(4 M + b[a]) I \[Omega] + (8 M \[Omega] + 2 I)/b[a] (2 M^2 \[Omega] - a m);
 \[Gamma]freq[i_, \[Omega]_, s_, m_, a_]:= i^2 + (s - 6 M I \[Omega] - 2 I/b[a] (2 M^2 \[Omega] - a m)) i - 8 M^2 \[Omega]^2 - 4 M I \[Omega] s - 8 M \[Omega]/b[a] (2 M^2 \[Omega] - a m);
@@ -40,11 +42,70 @@ b[a_] := b[a] = Sqrt[4 M^2 - 4 a^2];
 (* Initial guesses used as seeds in FindRoot *)
 (* Multiple asymptotic expansions are used for Schwarzschild, with different expansions providing better seeds for 
 different values of l and n *)
+
+(* l > n *)
 Schwarzfinit1[s_, l_, n_]:= ((I*(n+1/2)*(\[Beta][s]^2/27 + (\[Beta][s]*(1100*(n+1/2)^2 - 2719))/46656 + (11273136*(n+1/2)^4 - 52753800*(n+1/2)^2 + 66480535)/2902376448))/(l+1/2)^4 + 
    (-(\[Beta][s]^2/27) + (\[Beta][s]*(204*(n+1/2)^2 + 211))/3888 + (854160*(n+1/2)^4 - 1664760*(n+1/2)^2 - 776939)/40310784)/(l+1/2)^3 - (I*(n+1/2)*(\[Beta][s]/9 + (235*(n+1/2)^2)/3888 - 1415/15552))/(l+1/2)^2 + 
    (\[Beta][s]/3 - (5*(n+1/2)^2)/36 - 115/432)/(l+1/2) + (l+1/2) - I*(n+1/2))/(Sqrt[27]*M);
-    
+
+(* n \[GreaterEqual] 6, l\[LessEqual]2*)    
 Schwarzfinit2[n_] := Log[3]/(8 \[Pi] M) - I (n+1/2)/(4 M);
+
+(* Spectral method using hyperboloidal slicing *)
+(* n >= 6, 2<l<=n*)
+SpectralInitialGuess[s_, l_, n_]:= Module[{L1a, L1b, L2a, L2b, L2c, Prec, Ndiv, ndiv, \[Sigma], \[Sigma]0, \[Sigma]1, \[CapitalDelta]\[Sigma], x, Id, Z, c, d\[Sigma], D\[Sigma], D2\[Sigma], L1, L2, M, Eigens, Filtered},
+(* Operators appearing in the wave equation in coordinated (\[Tau], \[Sigma]), excluding radial derivatives *)
+L1a = -((2 \[Sigma])/(\[Sigma]+1));
+L1b = (1-2\[Sigma]^2)/(\[Sigma]+1);
+L2a = (-l(l+1) - \[Sigma](1-s^2))/(\[Sigma]+1);
+L2b = (\[Sigma](2-3\[Sigma]))/(\[Sigma]+1);
+L2c = (\[Sigma]^2 (1-\[Sigma]))/(\[Sigma]+1);
+
+Prec = 100; (*Numerical precision, machine precision found to be insufficient*)
+			(* Make optional for users to set? Requires more testing as well *)
+Ndiv = 5n; (* Subdivision of radial domain \[Sigma] \[Element] [0,1] used for discretization of radial derivs *)
+ndiv = Ndiv +1; (* Matrix dimension *)
+
+\[Sigma]0 = 0;
+\[Sigma]1 = 1;
+\[CapitalDelta]\[Sigma] = \[Sigma]1-\[Sigma]0;
+
+x[i_]:= Cos[(i \[Pi])/Ndiv]; (* Chebyshev points *)
+\[Sigma] = N[Table[\[Sigma]0 + \[CapitalDelta]\[Sigma]  (1+x[i])/2, {i, 0, Ndiv}], Prec]; (* Radial coordinate grid *)
+
+Id = IdentityMatrix[ndiv];
+Z = ConstantArray[0, {ndiv, ndiv}]; (* Zero matrix *)
+c[i_] := If[i(i-Ndiv) ==0, 2, 1];
+
+(* Radial derivative using Chebyshev-Lobatto method *)
+d\[Sigma][i_, j_] := 2/\[CapitalDelta]\[Sigma] Which[i==j && i == 0, (2 Ndiv^2+1)/6, i==j && i == Ndiv, -((2 Ndiv^2+1)/6), i==j,  -x[i]/(2(1-x[i]^2)) , i!=j, c[i]/c[j] (-1)^(i+j)/(x[i]-x[j])] ;
+
+(* First deriv matrix *)
+D\[Sigma]=N[Table[d\[Sigma][i, j], {i, 0, Ndiv}, {j, 0, Ndiv}], Prec];
+(* Second deriv matrix *)
+D2\[Sigma] = D\[Sigma].D\[Sigma];
+
+(* Full opertors in wave eq *)
+L1 = L1b D\[Sigma] + L1a Id;
+L2 = L2c D2\[Sigma] + L2b D\[Sigma] + L2a Id;
+
+M = ArrayFlatten[{{Z, Id}, {L2, L1}}];
+
+(* Solve for eigenvalues *)
+Eigens = Eigenvalues[M] ;
+
+(* Remove all values with Im[\[Omega]] = 0 *)
+Filtered = Reverse[DeleteCases[Eigens,x_/;(Im[x]>=0. )]];
+
+(*Pick out eigenvalue corresponding to the desired overtone *)
+(*Would be nice to save this and use the other elements for initial seeds for other values of n *)
+(*However, the accuracy decreases as one goes down the list *)
+I/4 Filtered[[n+1]]
+];
+
+
+
+
 
 (* For Kerr, an initial guess is needed for both the frequency, and the spheroidal eigevalue Alm *)
 
@@ -76,6 +137,8 @@ KerrAinit[s_, l_, m_, n_, a_] := (l+1/2)^2 - (a Kerrfinit[s, l, m, n, a])^2 /2 (
 (* The functions for the continued fraction provide the actual equations of which the QNMs and spheroidal eigenvalues are roots *)
 (* These functions allow for the n-th inversion to be easily identified, and make use of memoization to improve eficiency by
 avoiding repeated calls to the same functions. *)
+
+(*Would be nice to remove the While loops and replace with something like FixedPoint *)
 
 ContFrac[\[Omega]_,s_, l_, nInv_] := Module [{A,B,ak,bk,res=Indeterminate, j = nInv+1},
 A[(nInv+1)-2, \[Omega]]=1;
@@ -140,9 +203,11 @@ NInv = n;
 
 If[n >= 6 && 3 <= l && l < n, Message[QuasiNormalMode::SchwarzUntrusted, l, n]];
 
-If[l<= 2, finit = Schwarzfinit2[n], finit = Schwarzfinit1[s, l, n]];
-(*finit = Schwarzfinit1[s, l, n];*)
-(*Print[finit];*)
+(*Selection of initial seed method *)
+(*Ideally, we would first check how well this satisfies the eq to be solved.
+ If within the user's desired accuracy, just return the initial guess.
+ Would be faster *)
+Which[l< 2, finit = Schwarzfinit2[n], n < 6 && l >=  2, finit = Schwarzfinit1[s, l, n], n >=6 && l>=2 && l <=n, finit=SpectralInitialGuess[s, l, n], n >=6 && l>n, finit=Schwarzfinit1[s, l, n]];
 
 Sol = FindRoot[{Re[Leaver[x +I y, s, l, NInv]] == 0, Im[Leaver[x + I y, s, l, NInv]] == 0}, {x,Re[finit]}, {y, Im[finit]}];
 
@@ -192,6 +257,8 @@ If[Sol[[1]][[2]] < 0, freq = -Sol[[1]][[2]] + I Sol[[2]][[2]]]; (* Want to retur
 freq
 ];
 
+
+(*Consolidation of the above into a single function *)
 
 SyntaxInformation[QuasiNormalMode] = {ArgumentsPattern->{_, _, _, _, _}}; (* This specifies that QuasiNormalMode takes exactly 5 arguments*)
 
